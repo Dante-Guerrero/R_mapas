ef6f6b09c882a581abcb999eb402a1bbeefc8b11
Dante Guerrero <dante.guerrerobarreto@gmail.com>
2021-12-19 11:26:27 -0500

    Empezando a separar creadores para facilitar el mantenimiento

---
+++ auxiliares/creadores_de_data/demarcacion.R
@@ -0,0 +1,47 @@
+## DEMARCACION PAIS
+
+mx <- st_read("insumos/demarcacion/pais/geo.shp")
+mx_data <- select(as.data.frame(mx), DESCRIPCION=FILE)
+mx_geom <- st_geometry(mx)
+demarcacion_pais <- st_sf(mx_data, geometry = mx_geom)
+
+## DEMARCACION POR DEPARTAMENTO
+
+mx <- st_read("insumos/demarcacion/departamentos/geo.shp")
+mx_data <- select(as.data.frame(mx), DEPARTAMENTO=DEPARTAMEN, CAPITAL)
+mx_geom <- st_geometry(mx)
+demarcacion_departamentos <- st_sf(mx_data, geometry = mx_geom)
+
+## DEMARCACION POR PROVINCIA
+
+mx <- st_read("insumos/demarcacion/provincias/geo.shp")
+mx_data <- select(as.data.frame(mx), DEPARTAMENTO=DEPARTAMEN, PROVINCIA, CAPITAL)
+mx_geom <- st_geometry(mx)
+demarcacion_provincias <- st_sf(mx_data, geometry = mx_geom)
+
+## DEMARCACION POR DISTRITO
+
+mx <- st_read("insumos/demarcacion/distritos/geo.shp")
+mx_data <- select(as.data.frame(mx), DEPARTAMENTO = NOMBDEP, PROVINCIA= NOMBPROV, DISTRITO = NOMBDIST)
+mx_geom <- st_geometry(mx)
+demarcacion_distritos <- st_sf(mx_data, geometry = mx_geom)
+
+## DEMARCACION
+
+save(
+  demarcacion_pais,
+  demarcacion_departamentos,
+  demarcacion_provincias,
+  demarcacion_distritos,
+  file = "data/mapas/demarcacion.RData"
+)
+
+rm(
+  mx, 
+  mx_data, 
+  mx_geom,
+  demarcacion_pais,
+  demarcacion_departamentos,
+  demarcacion_provincias,
+  demarcacion_distritos
+)
--- auxiliares/descargar_insumos.R
+++ auxiliares/descargar_insumos.R
@@ -10,3 +10,5 @@ googledrive::drive_auth(email = "sefa@oefa.pe")
 drive_download("geometrias.zip", path="insumos/insumos.zip", overwrite = TRUE)
 unzip("insumos/insumos.zip", exdir = "insumos")
 file.remove("insumos/insumos.zip")
+
+# Nota: Los documentos que se descargan de Google Drive fueron obtenidos de la siguiente URL: https://www.geogpsperu.com/p/descargas.html
